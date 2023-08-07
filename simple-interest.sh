0612c62c027be45d6e1c9f76172df8bbc789b20e
MartinEncinas <132398568+MartinEncinas@users.noreply.github.com>
2023-08-06 23:58:51 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
  # annual rate of interest and time period in years.
  # Do not use this in production. Sample purpose only.
  # Author: Upkar Lidder (IBM)
- # Additional Authors:
- # <your GitHub username>
+ # Additional Authors: 
+ # MartinEncinas
  # Input:
  # p, principal amount
  # t, time period in years
